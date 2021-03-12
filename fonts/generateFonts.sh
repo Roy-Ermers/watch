c45cdbb85d7d1e37f63aa71ecd236c15593fae04
roy-ermers <dangergamesnl@gmail.com>
2021-03-12 21:20:38 +0100

    added

---
+++ fonts/generateFonts.sh
@@ -0,0 +1,3 @@
+npx lv_font_conv --size 72 --format lvgl --bpp 3 --font ./Rubik-Regular.ttf -o ./title.c --range 0x20-0x7F
+npx lv_font_conv --size 16 --format lvgl --bpp 3 --font ./Rubik-Regular.ttf -o ./regular.c --range 0x20-0x7F
+npx lv_font_conv --size 16 --format lvgl --bpp 3 --font ./Rubik-Medium.ttf -o ./subtitle.c --range 0x20-0x7F
